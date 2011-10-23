56580141984b380d81cb206cb1ca82073e0e06c7
d1ffuz0r <d1fffuz0r@gmail.com>
2011-10-23 05:43:59 +0400

    fucked tabs

--- Psto/Psto.py
+++ Psto/Psto.py
@@ -7,27 +7,27 @@ sys.path.append(path.abspath(curdir)+'\\xmpppy-0.5.0rc1-py2.5.egg')
 from xmpp import JID, Client, Message
 
 settings = {"jid": "",
-			"password": "",
-			"pstobot": "psto@psto.net",
-			"res": "Sublime Text Psto-Plugin"}
+            "password": "",
+            "pstobot": "psto@psto.net",
+            "res": "Sublime Text Psto-Plugin"}
 
 class PstoCommand(WindowCommand):
-	def __init__(self):
-		jid = JID(settings["jid"])
-		conn = Client(jid.getDomain(), debug=[])
-		if not conn.connect():
-			statusMessage("PSTO: connection error")
-		if not conn.auth(jid.getNode(), settings["password"], settings["res"]):
-			statusMessage("PSTO: autentification error")
-		self.conn = conn
+    def __init__(self):
+        jid = JID(settings["jid"])
+        conn = Client(jid.getDomain(), debug=[])
+        if not conn.connect():
+            statusMessage("PSTO: connection error")
+        if not conn.auth(jid.getNode(), settings["password"], settings["res"]):
+            statusMessage("PSTO: autentification error")
+        self.conn = conn
 
-	def run(self, window, args):
-		view = window.activeView()
-		window.showInputPanel("Message:", view.name(), partial(self.onDone), None, None)
+    def run(self, window, args):
+        view = window.activeView()
+        window.showInputPanel("Message:", view.name(), partial(self.onDone), None, None)
 
-	def onDone(self, text):
-		if text:
-			self.conn.send(Message(to=settings["pstobot"], body=text))
-			statusMessage("PSTO: Message sended (%s)" % text)
-		else:
-			statusMessage("PSTO: Error. Not message")
+    def onDone(self, text):
+        if text:
+            self.conn.send(Message(to=settings["pstobot"], body=text))
+            statusMessage("PSTO: Message sended (%s)" % text)
+        else:
+            statusMessage("PSTO: Error. Not message")
