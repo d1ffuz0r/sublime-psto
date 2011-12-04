74f7a98feeb54ed42508a200dc2f9d752df6df1d
d1ffuz0r <d1fffuz0r@gmail.com>
2011-12-04 19:50:19 +0300

    pep8

--- Psto/Psto.py
+++ Psto/Psto.py
@@ -3,13 +3,14 @@ from sublime import statusMessage
 from functools import partial
 from os import path, curdir
 import sys
-sys.path.append(path.abspath(curdir)+'\\xmpppy-0.5.0rc1-py2.5.egg')
+sys.path.append(path.abspath(curdir) + '\\xmpppy-0.5.0rc1-py2.5.egg')
 from xmpp import JID, Client, Message
 
 settings = {"jid": "",
             "password": "",
             "pstobot": "psto@psto.net",
             "res": "Sublime Text Psto-Plugin"}
+
 
 class PstoCommand(WindowCommand):
     def __init__(self):
@@ -23,7 +24,8 @@ class PstoCommand(WindowCommand):
 
     def run(self, window, args):
         view = window.activeView()
-        window.showInputPanel("Message:", view.name(), partial(self.onDone), None, None)
+        window.showInputPanel("Message:", view.name(),
+            partial(self.onDone), None, None)
 
     def onDone(self, text):
         if text:
